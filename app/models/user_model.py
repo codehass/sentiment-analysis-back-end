a15c156b97d5d29eb64f91392ad0f945b9570259
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-11-18 09:39:41 +0100

    create user model

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,1 +1,12 @@
-# user model file
+from sqlalchemy import Column, Integer, String, Boolean
+from db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = (Column(Integer, primary_key=True, index=True),)
+    username = Column(String, unique=True, nullable=False, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
