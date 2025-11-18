83b2148c9730ec214e7524bf9b2a68b86cb4383b
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-11-18 09:40:22 +0100

    create user schema

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,1 +1,19 @@
-# user schema file
+from pydantic import BaseModel
+from typing import Optional
+
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+
+    class Config:
+        orm_mode = True
