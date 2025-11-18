8a1444b0bc22507c036bb0c13426a88cb378321a
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-11-18 12:50:09 +0100

    refactor and add login endpoint

--- app/main.py
+++ app/main.py
@@ -1,16 +1,28 @@
+import os
 from fastapi import FastAPI, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from authentication.auth import get_password_hash
-from schemas.user_schema import UserSchema, UserCreate
+from authentication.auth import (
+    get_password_hash,
+    authenticate_user,
+    create_access_token,
+    get_current_user,
+)
+from schemas.user_schema import UserSchema, UserCreate, TokenSchema
 from models.user_model import User
 from db.database import engine, Base, get_db
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 
-@app.post("/users/", response_model=UserSchema)
+@app.post("/auth/register/", response_model=UserSchema)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.email == user.email).first()
 
@@ -27,6 +39,32 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return db_user
 
 
+@app.post("/auth/login/", response_model=TokenSchema)
+def login_for_access_token(
+    db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()
+):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="INcorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me", response_model=UserSchema)
+async def read_users_me(current_user: UserSchema = Depends(get_current_user)):
+    return current_user
+
+
 @app.get("/")
-def get_home():
-    return {"message": "Hello from backend"}
+def get_home(current_user: dict = Depends(get_current_user)):
+    if current_user:
+        return {"message": "Hello to the protected route!!"}
